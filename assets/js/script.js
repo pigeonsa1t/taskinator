a0906351736fb99a702ad601c9d3099ab5b1975c
pigeonsa1t <bowen_samantha@yahoo.com>
2022-04-11 14:20:23 -0400

    add ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
+//________________________________________________________________________________//
+// GLOBAL VARIABLES:
+
+// the buttonEl variable refers to DOM id="save-task"
 var buttonEl = document.querySelector("#save-task");
+// the tasksToDoEl variable refers to DOM id="tasks-to=do"
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+// on button click, create a task
 var createTaskHandler = function() {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
@@ -9,3 +15,13 @@ var createTaskHandler = function() {
 };
 
 buttonEl.addEventListener("click", createTaskHandler);
+
+//________________________________________________________________________________//
+// FUNCTIONS: //
+
+
+//________________________________________________________________________________//
+// EXPORTS: //
+
+
+//________________________________________________________________________________//
