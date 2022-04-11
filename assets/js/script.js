0537323f30b10cb1fddbf45662a036bf34b40a86
pigeonsa1t <bowen_samantha@yahoo.com>
2022-04-11 16:26:31 -0400

    added form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,25 @@
 //________________________________________________________________________________//
 // GLOBAL VARIABLES:
 
-// the buttonEl variable refers to DOM id="save-task"
-var buttonEl = document.querySelector("#save-task");
+// this variable now refers to the entire form element containing the button:
+var formEl = document.querySelector('#task-form')
 // the tasksToDoEl variable refers to DOM id="tasks-to=do"
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // on button click, create a task
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+// The best way to explore the data provided to an event object 
+  // is to use console.log(event); in the function handling the event.
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+// find and interact with the DOM form element:
+formEl.addEventListener("submit", createTaskHandler);
 
 //________________________________________________________________________________//
 // FUNCTIONS: //
@@ -25,3 +30,9 @@ buttonEl.addEventListener("click", createTaskHandler);
 
 
 //________________________________________________________________________________//
+
+
+// To begin, we'll move the event listener from the <button> element that we added in 
+// the last lesson and apply it to the <form> element itself. Now the browser will be able 
+// to listen to an event happening on the entire form rather than just the button.
+
